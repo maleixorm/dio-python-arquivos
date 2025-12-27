dbd752a36e12569bba2ff498b2e2e1739a9b7a30
Marcos Aleixo <maknytz@gmail.com>
2025-12-27 11:11:36 -0300

    Usando listas para ler o arquivo

--- 01_operacao_leitura.py
+++ 01_operacao_leitura.py
@@ -1,5 +1,7 @@
 arquivo = open('lorem.txt', 'r')
 # print(arquivo.read())
 # print(arquivo.readline())
-print(arquivo.readlines())
+# print(arquivo.readlines())
+while len(linha := arquivo.readline()):
+    print(linha)
 arquivo.close()
